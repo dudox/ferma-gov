4f9b9f370cda350e858cc69f0f67f694124a5356
dudox <trybemark@gmail.com>
2020-06-18 13:57:46 +0100

    implemented template design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,117 +1,220 @@
+
 <!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>
-    {{env('APP_NAME')}}
-  </title>
-  <!-- Favicon -->
-  <link href="../assets/img/brand/logo.png" rel="icon" type="image/png">
-  <!-- Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
-  <!-- Icons -->
-  <link href="../assets/js/plugins/nucleo/css/nucleo.css" rel="stylesheet" />
-  <link href="../assets/js/plugins/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet" />
-  <!-- CSS Files -->
-  <link href="../assets/css/argon-dashboard.css?v=1.1.0" rel="stylesheet" />
-</head>
+<html lang="en" >
+    <!--begin::Head-->
+    <head>
+        <meta charset="utf-8"/>
+        <title>Metronic | Login Page 3</title>
+        <meta name="description" content="Login page example"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
 
-<body class="bg-gradient-primary">
-  <div class="main-content py-5">
-    <!-- Navbar -->
-    <!-- Header -->
-    <div class="container mt--1">
-    <div class="header-body text-center mb-0 py-7">
-        <div class="row justify-content-center">
-        <div class="col-lg-12 col-md-12">
-            <img src="../assets/img/brand/blue.svg" width="150em" height="150em" />
-            <h1 class="text-white" style="position: relative; z-index: 100;"> {{env('APP_NAME')}} </h1>
+        <link href="{{asset('dash/css/calendar.css')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('dash/css/bootstrap.min.css?v=7.0.5')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('dash/css/prismjs.css?v=7.0.5')}}" rel="stylesheet" type="text/css" />
+        <link href="{{asset('dash/css/ferma-dashboard.css?v=7.0.5')}}" rel="stylesheet" type="text/css" />
+
+        <link rel="shortcut icon" href="https://keenthemes.com/metronic/themes/metronic/theme/html/demo9/dist/assets/media/logos/favicon.ico" />
+
+        <!-- Hotjar Tracking Code for keenthemes.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1070954,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-37564768-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-37564768-1');
+</script>    </head>
+    <!--end::Head-->
+
+    <style>
+        .fv-help-block {
+            text-align: left !important;
+            font-weight: 500 !important;
+            margin: 0% !important;
+            padding: 0% !important;
+        }
+
+        #slidecaption {
+            -webkit-animation-name: spinner;
+            -webkit-animation-timing-function: linear;
+            -webkit-animation-iteration-count: infinite;
+            -webkit-animation-duration: 5s;
+            animation-name: spinner;
+            animation-timing-function: linear;
+            animation-iteration-count: infinite;
+            animation-duration: 5s;
+            -webkit-transform-style: preserve-3d;
+            -moz-transform-style: preserve-3d;
+            -ms-transform-style: preserve-3d;
+            transform-style: preserve-3d;
+            }
+
+
+            /* WebKit and Opera browsers */
+            @-webkit-keyframes spinner {
+            from
+            {
+                -webkit-transform: rotateY(0deg);
+            }
+            to {
+                -webkit-transform: rotateY(-360deg);
+            }
+            }
+            /* all other browsers */
+            @keyframes spinner {
+            from {
+                -moz-transform: rotateY(0deg);
+                -ms-transform: rotateY(0deg);
+                transform: rotateY(0deg);
+            }
+            to
+            {
+                -moz-transform: rotateY(-360deg);
+                -ms-transform: rotateY(-360deg);
+                transform: rotateY(-360deg);
+
+            }
+            }
+    </style>
+
+    <!--begin::Body-->
+    <body  id="kt_body"  class="header-fixed header-mobile-fixed subheader-enabled page-loading"  >
+
+    	<!--begin::Main-->
+	<div class="d-flex flex-column flex-root">
+		<!--begin::Login-->
+<div class="login login-3 login-signin-on d-flex flex-row-fluid" id="kt_login">
+	<div class="d-flex flex-center bgi-size-cover bgi-no-repeat flex-row-fluid" style="background: #36b883;">
+		<div class="login-form text-center text-white p-7 position-relative overflow-hidden">
+			<!--begin::Login Header-->
+			<div class="d-flex flex-center mb-15">
+				<a href="#">
+                    <img id="slidecaption" src="../assets/img/brand/blue.svg"  class="max-h-100px" alt=""/>
+				</a>
+			</div>
+			<!--end::Login Header-->
+
+			<!--begin::Login Sign in form-->
+			<div class="login-signin">
+                <form class="form" id="kt_login_signin_form" role="form" method="POST" action="{{route('signin')}}">
+                    @csrf
+					<div class="form-group">
+						<input class="form-control h-auto text-white placeholder-white  bg-dark-o-70  border-0 py-4 px-8 mb-2" type="email" placeholder="Email" name="email" autocomplete="off"/>
+					</div>
+					<div class="form-group">
+						<input class="form-control h-auto text-white placeholder-white  bg-dark-o-70  border-0 py-4 px-8 mb-2" type="password" placeholder="Password" name="password"/>
+					</div>
+					<div class="form-group d-flex flex-wrap justify-content-between align-items-center">
+						<div class="checkbox-inline">
+							<label class="checkbox checkbox-outline checkbox-white text-white m-0">
+								<input type="checkbox" name="remember"/>
+								<span></span>
+								Remember me
+							</label>
+						</div>
+						<a href="javascript:;" id="kt_login_forgot" class="text-white font-weight-bold">Forget Password ?</a>
+					</div>
+					<div class="form-group text-center mt-5 ">
+						<button id="kt_login_signin_submit" class="btn btn-block btn-outline-white font-weight-bold opacity-90 py-3">Sign In</button>
+					</div>
+				</form>
+			</div>
+             <!--begin::Content footer-->
+
         </div>
-        </div>
+
     </div>
-    </div>
 
-    <!-- Page content -->
-    <div class="container mt--6">
-      <div class="row justify-content-center">
-        <div class="col-lg-5 col-md-7">
-          <div class="card bg-primary" style="border: none;z-index: 10;">
-            <div class="card-body px-lg-5 py-lg-5">
-              <form role="form" method="POST" action="{{route('signin')}}">
-                @csrf
-                <div>
-                </div>
-                <div class="form-group mb-3">
-                  <div class="input-group input-group-alternative">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="ni ni-email-83"></i></span>
-                    </div>
-                    <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="Email" type="email" name="email" required>
-                        @if ($errors->has('email'))
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="input-group input-group-alternative">
-                    <div class="input-group-prepend">
-                      <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
-                    </div>
-                    <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="Password" type="password" name="password" required>
-                    @if ($errors->has('password'))
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                    @endif
-                  </div>
-                </div>
-                <div class="text-center">
-                  <button type="submit" class="btn btn-white my-4 text-primary">Sign in</button>
-                </div>
-              </form>
-            </div>
-          </div>
-          {{-- <div class="row mt-3">
-            <div class="col-6">
-              <a href="#" class="text-light"><small>Forgot password?</small></a>
-            </div>
-            <div class="col-6 text-right">
-              <a href="#" class="text-light"><small>Create new account</small></a>
-            </div> --}}
-          </div>
-        </div>
-      </div>
-    </div>
-    <footer style="position:fixed; left: 2%; bottom: 2%; width: 100%;">
-      <div class="">
-        <div class="row">
-          <div class="col-xl-6" style="">
-            <div class="copyright text-xl-left text-white">
-              © {{date("Y", time())}} <a href="https://www.mayapro1.com" class="font-weight-bold ml-1 text-white" target="_blank">Mayapro1 Limited</a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-  </div>
-  <!--   Core   -->
-  <script src="../assets/js/plugins/jquery/dist/jquery.min.js"></script>
-  <script src="../assets/js/plugins/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-  <!--   Optional JS   -->
-  <!--   Argon JS   -->
-  <script src="../assets/js/argon-dashboard.min.js?v=1.1.0"></script>
-  <script src="https://cdn.trackjs.com/agent/v3/latest/t.js"></script>
-  <script>
-    window.TrackJS &&
-      TrackJS.install({
-        token: "ee6fab19c5a04ac1a32a645abde4613a",
-        application: "argon-dashboard-free"
-      });
-  </script>
-</body>
+</div>
 
+<!--end::Login-->
+    </div>
+    <div class="position-absolute bottom-0 text-center w-100 py-3">
+        <a href="#" class="text-white text-center font-size-h5">&copy; Copyright Smart Sahara</a>
+    </div>
+<!--end::Main-->
+
+
+        <script>var HOST_URL = "/metronic/tools/preview";</script>
+        <!--begin::Global Config(global config for global JS scripts)-->
+        <script>
+            var KTAppSettings = {
+    "breakpoints": {
+        "sm": 576,
+        "md": 768,
+        "lg": 992,
+        "xl": 1200,
+        "xxl": 1200
+    },
+    "colors": {
+        "theme": {
+            "base": {
+                "white": "#ffffff",
+                "primary": "#0BB783",
+                "secondary": "#E5EAEE",
+                "success": "#1BC5BD",
+                "info": "#8950FC",
+                "warning": "#FFA800",
+                "danger": "#F64E60",
+                "light": "#F3F6F9",
+                "dark": "#212121"
+            },
+            "light": {
+                "white": "#ffffff",
+                "primary": "#D7F9EF",
+                "secondary": "#ECF0F3",
+                "success": "#C9F7F5",
+                "info": "#EEE5FF",
+                "warning": "#FFF4DE",
+                "danger": "#FFE2E5",
+                "light": "#F3F6F9",
+                "dark": "#D6D6E0"
+            },
+            "inverse": {
+                "white": "#ffffff",
+                "primary": "#ffffff",
+                "secondary": "#212121",
+                "success": "#ffffff",
+                "info": "#ffffff",
+                "warning": "#ffffff",
+                "danger": "#ffffff",
+                "light": "#464E5F",
+                "dark": "#ffffff"
+            }
+        },
+        "gray": {
+            "gray-100": "#F3F6F9",
+            "gray-200": "#ECF0F3",
+            "gray-300": "#E5EAEE",
+            "gray-400": "#D6D6E0",
+            "gray-500": "#B5B5C3",
+            "gray-600": "#80808F",
+            "gray-700": "#464E5F",
+            "gray-800": "#1B283F",
+            "gray-900": "#212121"
+        }
+    },
+    "font-family": "Poppins"
+};
+        </script>
+    <script src="https://keenthemes.com/metronic/themes/metronic/theme/html/demo9/dist/assets/plugins/global/plugins.bundle.js?v=7.0.5"></script>
+    <script src="https://keenthemes.com/metronic/themes/metronic/theme/html/demo9/dist/assets/plugins/custom/prismjs/prismjs.bundle.js?v=7.0.5"></script>
+    <script src="https://keenthemes.com/metronic/themes/metronic/theme/html/demo9/dist/assets/js/scripts.bundle.js?v=7.0.5"></script>
+    <script src="https://keenthemes.com/metronic/themes/metronic/theme/html/demo9/dist/assets/plugins/custom/fullcalendar/fullcalendar.bundle.js?v=7.0.5"></script>
+    <script src="{{asset('dash/js/auth/login.js')}}"></script>
+                        <!--end::Page Scripts-->
+            </body>
+    <!--end::Body-->
 </html>
