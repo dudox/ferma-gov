43ecc77502a4dcbbc78b2e36f372cddbe76d5ea7
dudox <trybemark@gmail.com>
2020-06-26 09:50:55 +0100

    fix broken image link

--- app/Http/Controllers/API/Process.php
+++ app/Http/Controllers/API/Process.php
@@ -93,7 +93,7 @@ class Process extends Controller
 
         if($this->count == 7){
             if($this->text[6] == 1){
-                $this->client->messages->create($this->convert($this->phone),['from' => 'FERMA', 'body' => "Thank you for reporting this road to FERMA. Please visit the link below to upload an image ".$this->url] );
+                $this->client->messages->create($this->convert($this->phone),['from' => 'FERMA', 'body' => "Thank you for reporting this road to FERMA. Click on the link below to upload an road image  ".$this->url] );
                 $this->storeInput();
             }
             elseif($this->text[6] == 2){
